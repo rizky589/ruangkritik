01918e8cbc7edf32e33c9c2929f2ac865742daa3
iki <rizkyananda954@gmail.com>
2026-04-27 08:09:01 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import ProtectedRoute from './components/ProtectedRoute'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import CreateTopic from './pages/CreateTopic'
-import TopicDetail from './pages/TopicDetail'
-import Profile from './pages/Profile'
-import Admin from './pages/Admin'
-
-export default function App() {
+function App() {
   return (
-    <div className="app-wrapper">
-      <Navbar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/topic/:id" element={<TopicDetail />} />
-          <Route path="/create-topic" element={
-            <ProtectedRoute><CreateTopic /></ProtectedRoute>
-          } />
-          <Route path="/profile" element={
-            <ProtectedRoute><Profile /></ProtectedRoute>
-          } />
-          <Route path="/admin" element={
-            <ProtectedRoute><Admin /></ProtectedRoute>
-          } />
-        </Routes>
-      </main>
-      <Footer />
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#0f172a",
+      color: "white",
+      fontSize: "24px",
+      textAlign: "center"
+    }}>
       Website sedang maintenance 🚧
     </div>
-  )
+  );
 }
+
+export default App;
